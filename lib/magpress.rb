5648a6b52512a48e37113b1de2285b61c944f841
Amit Patel <amit.patel@botreeconsulting.com>
2016-12-29 11:50:46 +0530

    Fixing class lookup errors

--- lib/magpress.rb
+++ lib/magpress.rb
@@ -1,5 +1,11 @@
-require "magpress/version"
-require "magpress/client"
+require 'magpress/version'
+require 'magpress/client'
+require 'magpress/error'
+require 'magpress/login'
+require 'magpress/post'
+require 'magpress/image'
+require 'magpress/tag'
+require 'magpress/author'
 
 module Magpress
   # Your code goes here...
